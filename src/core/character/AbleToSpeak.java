50e208e5c48f998535718c14ac4c0c06b5ff2e5b
Fr-Steduthu <75694994+Fr-Steduthu@users.noreply.github.com>
2021-11-14 15:24:10 +0100

    Update AbleToSpeak.java

--- src/core/character/AbleToSpeak.java
+++ src/core/character/AbleToSpeak.java
@@ -2,5 +2,8 @@ package core.character;
 
 
 public interface AbleToSpeak extends Interactable {
+    public default void interact(){
+        this.speak();
+    }
     public abstract void speak();
 }
