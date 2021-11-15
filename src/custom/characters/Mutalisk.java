a1c9ab228dda033ffddf25a00e9b9206a640808a
Fr-Steduthu <erwannsaintgermain@gmail.com>
2021-11-15 09:22:36 +0100

    Upadate States + Custom

--- src/custom/characters/Mutalisk.java
+++ src/custom/characters/Mutalisk.java
@@ -2,11 +2,11 @@ package custom.characters;
 
 import core.character.Monster;
 
-public class Mustalisk extends Monster{
+public class Mutalisk extends Monster{
 
 	private static final long serialVersionUID = -6050530201684940352L;
 
-	public Mustalisk() {
+	public Mutalisk() {
 		super("Mutalisk", 20.0f, 0, 1.0f);
 	}
 
