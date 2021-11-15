3d2c1904b409c680246aec0bc2b296cf3c6952b4
Fr-Steduthu <erwannsaintgermain@gmail.com>
2021-11-15 09:55:45 +0100

    MAJ equipment + custom.monsters

--- src/core/character/NPC.java
+++ src/core/character/NPC.java
@@ -5,8 +5,15 @@ public class NPC extends Character{
 
 	private static final long serialVersionUID = 1134079016999922837L;
 
-    public NPC(String name, int inventoryCapacity, String dialogue) {
-        super(name, 20f, 0.0f, inventoryCapacity);
+    public NPC(String name, String dialogue, int inventoryCapacity, int equipment_capacity) {
+        super(name, 20f, 0.0f, inventoryCapacity, equipment_capacity);
+        this.dialogue = dialogue;
+        this.isLootable = true;
+        this.isAbleToSpeak = true;
+    }
+    
+    public NPC(String name, String dialogue, int inventoryCapacity) {
+        super(name, 20f, 0.0f, inventoryCapacity, 0);
         this.dialogue = dialogue;
         this.isLootable = true;
         this.isAbleToSpeak = true;
--- src/custom/characters/Hydralisk.java
+++ src/custom/characters/Hydralisk.java
@@ -8,7 +8,7 @@ public class Hydralisk extends Monster {
 	private static final long serialVersionUID = 6919651768245985734L;
 
 	public Hydralisk() {
-		super("Hydralisk", 20.0f, 0, 1.5f);
+		super("Hydralisk", 20.0f, 1.5f, 0);
 		this.currentState = State.IMMUNE_ALL;
 	}
 
