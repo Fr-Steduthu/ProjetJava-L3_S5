685c29bac87086856c24842b58121d65a6af76da
Fr-Steduthu <erwannsaintgermain@gmail.com>
2021-11-23 10:48:52 +0100

    Smol fixiz

--- src/builtin/places/RandomItemRoom.java
+++ src/builtin/places/RandomItemRoom.java
@@ -4,4 +4,11 @@ import core.places.Place;
 
 public class RandomItemRoom extends Place {
 
+	private static final long serialVersionUID = 5901191614150277973L;
+
+	public RandomItemRoom(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
 }
