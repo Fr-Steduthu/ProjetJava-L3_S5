13ec24968f7a8d05dc8806bbacc3f5fb793dc8b8
Creerio <10045021-Creerio@users.noreply.gitlab.com>
2021-11-26 11:20:05 +0100

    Updated TutoMan
    He now has an interaction setup with a random useless tip given to the player.
    
    Signed-off-by: Creerio <10045021-Creerio@users.noreply.gitlab.com>

--- src/builtin/characters/npcs/TutoMan.java
+++ src/builtin/characters/npcs/TutoMan.java
@@ -2,14 +2,24 @@ package builtin.characters.npcs;
 
 import builtin.items.TutoStone;
 import core.character.NPC;
+import hmi.HMI;
 
 public class TutoMan extends NPC {
 
-	private static final long serialVersionUID = 1443591607874829245L;
+    private static final long serialVersionUID = 1443591607874829245L;
+    
+    private final String[] tipsArray = new String[]{"Using HELP gives you all available commands", "ATTACKing a Character decreases it's health points", "You can TAKE items", "Nah. You'll be fine without my advices"};
 
-	public TutoMan() {
-        super("Tuto-MAN", "TODO : THIS IS A PLACEHOLDER FOR NOW", 1);
-        this.inventory.addItem(new TutoStone());
+    public TutoMan() {
+        super("Tuto-MAN", "I don't know if you know but..", 1);
     }
     
+    @Override
+    public void interact() {
+        int tipSelected = (int) (Math.random() * (tipsArray.length - 0));
+        this.speak();
+        HMI.message(tipsArray[tipSelected]);
+        this.getLocation().addItem(new TutoStone());
+        this.getLocation().removeNpc(this);
+    }  
 }
