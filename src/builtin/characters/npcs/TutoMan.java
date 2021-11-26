b4d17303d75b0fd749960bc84633e9a0b72e784b
Creerio <10045021-Creerio@users.noreply.gitlab.com>
2021-11-26 13:19:15 +0100

    Added a tip to TutoMan's tip list
    Signed-off-by: Creerio <10045021-Creerio@users.noreply.gitlab.com>

--- src/builtin/characters/npcs/TutoMan.java
+++ src/builtin/characters/npcs/TutoMan.java
@@ -9,7 +9,7 @@ public class TutoMan extends NPC {
 
     private static final long serialVersionUID = 1443591607874829245L;
     
-    private final String[] tipsArray = new String[]{"Using HELP gives you all available commands", "ATTACKing a Character decreases it's health points", "You can TAKE items", "Nah. You'll be fine without my advices"};
+    private final String[] tipsArray = new String[]{"Using HELP gives you all available commands", "ATTACKing a Character decreases it's health points", "You can TAKE items", "INTERACTing with some characters may help you", "Nah. You'll be fine without my advices"};
 
     public TutoMan() {
         super("Tuto-MAN", "I don't know if you know but..", 1);
