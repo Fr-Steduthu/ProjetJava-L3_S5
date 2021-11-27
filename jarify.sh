d8b39992c1389d3358e6524c8139931c9a95232c
Creerio <10045021-Creerio@users.noreply.gitlab.com>
2021-11-27 13:48:10 +0100

    Updated jarify.sh
    The script is now able to generate a full jar executable, it depends however on the manifest.mf file now
    Will have to be finished later though
    
    Signed-off-by: Creerio <10045021-Creerio@users.noreply.gitlab.com>

--- jarify.sh
+++ jarify.sh
@@ -1,2 +1,24 @@
-cd bin/
-jar -f executable.jar -e core.Main -c core builtin custom HMI
+
+# TODO : Check java version, check manifest file presence
+
+
+# Prepare a build folder for every single class files
+mkdir build
+cp -r src/main/* build/
+cd build/
+
+# Creates all class files
+javac -cp . core/Main.java
+
+# Removes all java files
+find . -name "*.java" -type f -delete
+
+# Let some time to the system to have the java files removed and the class files created
+sleep 1
+
+# Creates the jar file and put it in our current folder
+jar -cfm executable.jar ../manifest.mf .
+cp executable.jar ../
+
+cd ../
+rm -r build
