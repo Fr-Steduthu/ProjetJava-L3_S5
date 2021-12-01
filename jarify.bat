8d20b034effd617175bbc14bbdea713b9f3c74d0
Fr-Steduthu <erwannsaintgermain@gmail.com>
2021-12-01 15:08:56 +0100

    Update jarify.bat

--- jarify.bat
+++ jarify.bat
@@ -9,12 +9,11 @@ setlocal EnableDelayedExpansion
 MKDIR %cd%\build
 
 C:\Languages\Java\8JDK291\bin\javac src/core/Main.java -cp src/ -d build/
-echo "Manifest-Version: 1.0!\n!Main-Class: core.Main" > manifest.mf
 
 cd %cd%\build
+echo Manifest-Version: 1.0!\n!Main-Class: core.Main > manifest.mf
 C:\Languages\Java\8JDK291\bin\jar -cfm ..\game.jar ..\manifest.mf .
 cd..
 
 RMDIR %cd%\build /S
-del /f manifest.mf
 pause
