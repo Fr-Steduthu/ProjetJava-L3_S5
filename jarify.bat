5785ae71eac896b6e6da9b4932a25f2ff9d2f06b
Creerio <10045021-Creerio@users.noreply.gitlab.com>
2021-12-01 15:00:46 +0100

    Updated jarify batch script
    Might work, or not
    
    Signed-off-by: Creerio <10045021-Creerio@users.noreply.gitlab.com>

--- jarify.bat
+++ jarify.bat
@@ -1,12 +1,20 @@
 @echo off
 echo %JAVA_HOME% = "C:\Program Files\Java\jdk1.6\bin"
 
+setlocal EnableDelayedExpansion
+(set \n=^
+%=Do not remove this line=%
+)
 
 MKDIR %cd%\build
 
 C:\Languages\Java\8JDK291\bin\javac src/core/Main.java -cp src/ -d build/
+echo "Manifest-Version: 1.0!\n!Main-Class: core.Main" > manifest.mf
 
-C:\Languages\Java\8JDK291\bin\jar cf game.jar -M core/Main -C ./
+cd %cd%\build
+C:\Languages\Java\8JDK291\bin\jar -cfm ../game.jar ../manifest.mf .
+cd..
 
 RMDIR %cd%\build /S
+del /f manifest.mf
 pause
