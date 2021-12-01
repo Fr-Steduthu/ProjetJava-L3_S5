152b9001633d458f91bc30759288a8681811be2f
Creerio <10045021-Creerio@users.noreply.gitlab.com>
2021-12-01 13:23:49 +0100

    Added javadoc for item package
    Signed-off-by: Creerio <10045021-Creerio@users.noreply.gitlab.com>

--- src/core/item/Item.java
+++ src/core/item/Item.java
@@ -10,41 +10,104 @@ import core.place.Place;
 public abstract class Item implements Serializable{
 	private static final long serialVersionUID = -2738172528031259592L;
 	
+        /**
+         * Item name
+         */
 	private String name; //Variable pour ne pas avoir a recreer l'objet apres "identification"
+        
+        /**
+         * Item location, can be a Place or an Inventory
+         */
 	private Object location = null;
 
+        /**
+         * Can the item be taken from the room
+         */
 	protected boolean isTakable = true;
+        
+        /**
+         * Can the item be taken right now from the room
+         */
 	protected boolean isCurrentlyTakable = true;
+        
+        /**
+         * Can the item be used
+         */
 	protected boolean isUsable = false;
+        
+        /**
+         * Can the item be used right now
+         */
 	protected boolean isCurrentlyUsable = false;
 
 
+        /**
+         * Default item constructor
+         * 
+         * @param name 
+         * The item's name
+         */
 	public Item(String name) {
 		this.name = name;
 	}
 	
+        /**
+         * @return the item's name
+         */
 	public final String getName() {
 		return this.name;
 	}
         
+        /**
+         * Sets a new item name
+         * 
+         * @param newName 
+         * The new name
+         */
 	public final void setName(String newName) {
 		this.name = newName;
 	}
 	
+        /**
+         * Sets a new inventory location for the item
+         * 
+         * @param inventory 
+         * The inventory
+         */
 	public final void setLocation(Inventory inventory) {
 		this.location = inventory;
 	}
         
+        /**
+         * Sets a new place location for the item
+         * 
+         * @param location 
+         * The place
+         */
 	public final void setLocation(Place location) {
 		this.location = location;
 	}
         
+        /**
+         * @return the item's current location
+         */
 	public final Object getLocation() {
 		return this.location;
 	}
 	
+        /**
+         * @return a readable string giving info on the item
+         */
 	public abstract String look();
 	
+        /**
+         * Gives an item to the player
+         * 
+         * @param player
+         * The player who will receive the item
+         * 
+         * @return if the item was taken
+         */
 	public final boolean giveTo(Player player) {
 		if(this.isTakable) {
 			if(this.isCurrentlyTakable) {
@@ -61,6 +124,14 @@ public abstract class Item implements Serializable{
 		return false;
 	}
 	
+        /**
+         * Uses an item on a target
+         * 
+         * @param target
+         * The target, can be anything
+         * 
+         * @return if item was used
+         */
 	public /*final*/ boolean use(Object target) {
 		HMI.debug(this.isUsable +"" + this.isCurrentlyUsable);
 		if(this.isUsable) {
@@ -76,13 +147,27 @@ public abstract class Item implements Serializable{
 		return false;
 	}
 
+        /**
+         * What to do on the item use
+         * 
+         * @param target
+         * The target
+         * 
+         * @return if the item was used
+         */
 	protected abstract boolean onUse(Object target);
         
+    /**
+     * @return the item name
+     */
     @Override
     public String toString() {
         return this.name;
     }
 
+        /**
+         * @return if the item needs a target
+         */
 	public boolean needsTarget() {
 		return false;
 	}
--- src/core/item/Usable.java
+++ src/core/item/Usable.java
@@ -4,18 +4,32 @@ public abstract class Usable extends Item {
 
 	private static final long serialVersionUID = 8588907239114065167L;
 
+        /**
+         * Default constructor for an usable item
+         * 
+         * @param name 
+         * The item's name
+         */
 	public Usable(String name) {
 		super(name);
 		this.isCurrentlyUsable = true;
 		this.isUsable = true;
 	}
 
+        /**
+         * @return a readable string giving info on the item
+         */
 	@Override
-	public String look() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public abstract String look();
 
+        /**
+         * What to do on the item use
+         * 
+         * @param target
+         * The target
+         * 
+         * @return if the item was used
+         */
 	@Override
 	protected abstract boolean onUse(Object target);
 
