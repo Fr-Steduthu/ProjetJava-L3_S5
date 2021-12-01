52525b9eb1985505d30665e613d624cea800cb13
Fr-Steduthu <erwannsaintgermain@gmail.com>
2021-12-01 14:06:36 +0100

    Update jarify.sh

--- jarify.sh
+++ jarify.sh
@@ -3,6 +3,6 @@
 mkdir build/
 javac src/core/Main.java -cp src/ -d build/
 cd build/
-jar --create --file game.jar --main-class core/Main -c ./
-cd ../
-rm -r build/
+jar --create -f game.jar --main-class core/Main -c ./
+mv game.jar ../
+rm -r ../build/
