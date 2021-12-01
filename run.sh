8d85fc383e4ce0fd4fc0e5d65cf9c69f7fd11727
Creerio <10045021-Creerio@users.noreply.gitlab.com>
2021-12-01 14:19:20 +0100

    Updated TODO and run script
    Run now uses the same javaChecker function
    
    Signed-off-by: Creerio <10045021-Creerio@users.noreply.gitlab.com>

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-fileName="executable.jar"
+fileName="game.jar"
 
+# Checks for the right java version.. if java is even installed
 javaChecker() {
+  if ! command -v java &> /dev/null
+  then
+    whiptail --title "Java might not be installed" --msgbox "It seems you might not have installed java.\nPlease install it before running this script.\n\nExiting script." 0 0
+    exit 1
+  fi
+
   java=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
   java=$((java + 0)) # We kind of need an integer so..
   if [ $java -gt 11 ]; then
     whiptail --title "Wrong java version detected" --msgbox "It seems you are using java $java according to the \"java -version\" command.\nPlease change your java version temporarily to execute this script.\nOne way to do that is to locate your java version and execute in your terminal a modified version of this command :\nexport PATH=\"/usr/lib/jvm/java-8-openjdk/bin/:\$PATH\"\n\nExiting script." 0 0
-    exit 1
+    exit 2
   fi
 }
 
@@ -18,6 +25,5 @@ if ! [ -e $fileName ]; then
   exit 2
 fi
 
-echo "Executing $fileName"
+
 java -jar $fileName
-echo "End of the script. Thanks for using it !"
